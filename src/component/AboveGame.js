4a3040c98ffcd1cf070059e8975845ca84a45edd
김재만 <daga4242@gmail.com>
2022-06-14 18:58:08 +0900

    feature

---
+++ src/component/AboveGame.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function AboveGame() {
+  return (
+    <div className="above-game">
+      <a className="game-intro">
+        Join the numbers and get to the <strong>2048 title</strong>
+      </a>
+      <a href="/" className="restart-button">
+        New Game
+      </a>
+    </div>
+  );
+}
---
+++ src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Header() {
+  return (
+    <div className="heading">
+      <h1 className="title">2048</h1>
+      <div className="scores-container">
+        <div className="score-container">
+          364<div className="score-addition">+20</div>
+        </div>
+        <div className="best-container">423</div>
+      </div>
+    </div>
+  );
+}
